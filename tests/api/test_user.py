763587fc1363e308fba9248a1696aafd97af0df0
bogay <pojay11523@gmail.com>
2023-05-26 15:47:44 +0800

    test: singup multiple users

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -1,6 +1,8 @@
 from fastapi import status
 from fastapi.testclient import TestClient
 
+from meter.domain.user import UserSignup
+
 
 def test_signup_and_login(test_client: TestClient):
     resp = test_client.post(
@@ -20,3 +22,24 @@ def test_signup_and_login(test_client: TestClient):
         },
     )
     assert resp.status_code == status.HTTP_200_OK, resp.json()
+
+
+def test_signup_multiple_users(test_client: TestClient):
+    inputs = [
+        UserSignup(
+            name='bar',
+            email='bar@bar.com',
+            password='foo',
+        ),
+        UserSignup(
+            name='baz',
+            email='baz@bar.com',
+            password='foo',
+        )
+    ]
+    for i in inputs:
+        resp = test_client.post(
+            '/user/signup',
+            json=i.dict(),
+        )
+        assert resp.status_code == status.HTTP_201_CREATED, resp.json()
