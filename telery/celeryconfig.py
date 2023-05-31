fff7b9a12e31bd25017c8746059ff1237b92b468
Siao, Yu-Jie <a6194315535@gmail.com>
2023-06-01 05:08:59 +0800

    fix: use UTC, it will work. watame doesn't know

--- telery/celeryconfig.py
+++ telery/celeryconfig.py
@@ -6,26 +6,25 @@ result_backend = "rpc://"
 task_serializer = "json"
 result_serializer = "json"
 accept_content = ["json"]
-timezone = "Asia/Taipei"
-enable_utc = True
+# TODO: Asia/Taipei doesn't work
+timezone = "UTC"
 
 imports = ["telery.crawler_tasks"]
 
-# TODO: check why celery beat can't run with crontab
 beat_schedule = {
     "Earthquake": {
         "task": "telery.crawler_tasks.Earthquake",
-        "schedule": 300,
+        "schedule": crontab(minute="5"),
         "args": (2020, 5),
     },
     "Reservoir": {
         "task": "telery.crawler_tasks.Reservoir",
-        "schedule": 300,
+        "schedule": crontab(minute="5"),
         "args": (2020, 5, 1),
     },
     "Power": {
         "task": "telery.crawler_tasks.Power",
-        "schedule": 300,
+        "schedule": crontab(minute="5"),
         "args": (),
     },
 }
--- telery/submitter.py
+++ telery/submitter.py
@@ -25,6 +25,7 @@ if __name__ == "__main__":
     pprint(result.get())
 
 # TODO: docker-compose
-# celery -A tasks worker --loglevel=DEBUG
 # celery -A tasks worker --loglevel=DEBUG -B
+# celery -A telery worker --loglevel=INFO
+# celery -A telery beat --loglevel=INFO
 # docker run --rm -p 5672:5672 -p 15672:15672 rabbitmq:3.12-rc-management-alpine
