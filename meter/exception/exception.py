d9d40245c0d82412f948340aa214813ce409cc3b
bogay <pojay11523@gmail.com>
2023-06-07 02:04:40 +0800

    style(exception): use `fastapi.status` for status_code default
    for better readability.

--- meter/exception/exception.py
+++ meter/exception/exception.py
@@ -1,3 +1,5 @@
+from fastapi import status
+
 from meter.constant.response_code import ResponseCode
 
 
@@ -5,7 +7,7 @@ class CustomErrorException(Exception):
     def __init__(
         self,
         response_code: ResponseCode,
-        status_code: int = 400,
+        status_code: int = status.HTTP_400_BAD_REQUEST,
     ):
         self.response_code = response_code
         self.status_code = status_code
