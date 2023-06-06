e1306bc33fdd9b18a904c0492bc960db8aa35113
bogay <pojay11523@gmail.com>
2023-06-07 00:32:32 +0800

    feat: return error code when signup failed

--- meter/api/user.py
+++ meter/api/user.py
@@ -5,6 +5,9 @@ from pydantic import BaseModel
 
 from meter.api import get_current_user, get_user_service
 from meter.domain.user import User, UserService, UserSignup
+from meter.constant.response_code import ResponseCode
+from meter.helper import raise_custom_exception
+from sqlalchemy.exc import IntegrityError
 
 router = APIRouter()
 
@@ -18,7 +21,15 @@ async def signup(
     input: UserSignup,
     svc: Annotated[UserService, Depends(get_user_service)],
 ):
-    id = svc.signup(input)
+    try:
+        id = svc.signup(input)
+    except IntegrityError as e:
+        orig = str(e.orig)
+        if "name" in orig:
+            raise_custom_exception(ResponseCode.USER_SIGNUP_DUPLICATE_USERNAME_1301)
+        elif "email" in orig:
+            raise_custom_exception(ResponseCode.USER_SIGNUP_DUPLICATE_EMAIL_1302)
+        raise e
     return SignupResponse(id=id)
 
 
