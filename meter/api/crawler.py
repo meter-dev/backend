3fc6164c2daf24d7ed823b180ff66a12775fdc03
skps2010 <skps2010030908@yahoo.com>
2023-06-06 23:37:50 +0800

    return water based on location

--- meter/api/crawler.py
+++ meter/api/crawler.py
@@ -2,27 +2,37 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends
 
+from crawler.model import Dam, Eq, PowerReturn
 from meter.api import get_crawler_service
 from meter.domain.crawler import CrawlerService
 
 router = APIRouter()
 
 
-@router.get("/power")
+@router.get(
+    "/power",
+    response_model=list[PowerReturn],
+)
 async def get_all_power(
     svc: Annotated[CrawlerService, Depends(get_crawler_service)],
 ):
     return svc.get_all_power()
 
 
-@router.get("/eq")
+@router.get(
+    "/eq",
+    response_model=list[Eq],
+)
 async def get_all_eq(
     svc: Annotated[CrawlerService, Depends(get_crawler_service)],
 ):
     return svc.get_all_eq()
 
 
-@router.get("/dam")
+@router.get(
+    "/dam",
+    response_model=list[Dam],
+)
 async def get_all_dam(
     svc: Annotated[CrawlerService, Depends(get_crawler_service)],
 ):
--- meter/domain/crawler.py
+++ meter/domain/crawler.py
@@ -1,13 +1,13 @@
 from sqlmodel import Session, select
 
-from crawler.model import Dam, Eq, Power
+from crawler.model import Dam, Eq, Power, PowerReturn
 
 
 class CrawlerService:
     def __init__(self, session: Session) -> None:
         self.session = session
 
-    def get_all_power(self):
+    def get_all_power(self) -> list[PowerReturn]:
         powers = self.session.exec(select(Power)).all()
         new_powers = []
         for power in powers:
@@ -21,14 +21,34 @@ class CrawlerService:
                 whole["load"] += power[k]["load"]
                 whole["max_supply"] += power[k]["max_supply"]
 
-            whole["recv_rate"] = whole["max_supply"] / whole["load"] / whole["load"]
+            whole["recv_rate"] = (whole["max_supply"] - whole["load"]) / whole["load"]
             power["whole"] = whole
-            new_powers.append(power)
+            new_powers.append(PowerReturn(**power))
 
         return new_powers
 
-    def get_all_eq(self):
+    def get_all_eq(self) -> list[Eq]:
         return self.session.exec(select(Eq)).all()
 
-    def get_all_dam(self):
-        return self.session.exec(select(Dam)).all()
+    def get_all_dam(self) -> list[Dam]:
+        dams = self.session.exec(select(Dam)).all()
+        keys = [["石門", "寶山第二", "永和山"], ["鯉魚潭", "德基"], ["南化", "曾文", "烏山頭"]]
+        names = ["竹", "中", "南"]
+        for i in range(3):
+            storage = 0
+            all_storage = 0
+            for name in keys[i]:
+                dam = self.session.exec(
+                    select(Dam).where(Dam.name == f"{name}水庫").order_by(-Dam.timestamp)
+                ).first()
+                storage += dam.storage
+                all_storage += dam.storage / dam.percent
+            dams.append(
+                Dam(
+                    name=names[i],
+                    timestamp=0,
+                    storage=storage,
+                    percent=storage / all_storage,
+                )
+            )
+        return dams
