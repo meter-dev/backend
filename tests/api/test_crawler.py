c8f0291e6fd940a605cf4bf8bc8a29d028dbb016
skps2010 <skps2010030908@yahoo.com>
2023-06-06 20:48:14 +0800

    fix test

--- tests/api/test_crawler.py
+++ tests/api/test_crawler.py
@@ -8,14 +8,40 @@ from crawler.model import Dam, Eq, Power
 
 
 class TestCrawlerClass:
-    def setup_method(self, test_session: Session) -> None:
+    # TODO: make this auto-execute at the start of the testing
+    def load_data(self, test_session: Session) -> None:
         for c in (Power, Eq, Dam):
             report = json.load(open(f"crawler/example/{c.__name__.lower()}.json"))
             for r in report:
-                test_session().add(c(**r))
-                test_session().commit()
+                test_session.add(c(**r))
+                test_session.commit()
 
-    def test_get_power(self, test_client: TestClient):
-        # I don't know why it is not working
+    def test_get_power(self, test_client: TestClient, test_session: Session):
+        self.load_data(test_session)
+
         resp = test_client.get("/crawler/power")
+        assert resp.status_code == status.HTTP_200_OK, resp.json()
+        id = resp.json()[0]["id"]
+
+        resp = test_client.get(f"/crawler/power/{id}")
+        assert resp.status_code == status.HTTP_200_OK, resp.json()
+
+    def test_get_eq(self, test_client: TestClient, test_session: Session):
+        self.load_data(test_session)
+
+        resp = test_client.get("/crawler/eq")
+        assert resp.status_code == status.HTTP_200_OK, resp.json()
+        id = resp.json()[0]["id"]
+
+        resp = test_client.get(f"/crawler/eq/{id}")
+        assert resp.status_code == status.HTTP_200_OK, resp.json()
+
+    def test_get_dam(self, test_client: TestClient, test_session: Session):
+        self.load_data(test_session)
+
+        resp = test_client.get("/crawler/dam")
+        assert resp.status_code == status.HTTP_200_OK, resp.json()
+        id = resp.json()[0]["id"]
+
+        resp = test_client.get(f"/crawler/dam/{id}")
         assert resp.status_code == status.HTTP_200_OK, resp.json()
